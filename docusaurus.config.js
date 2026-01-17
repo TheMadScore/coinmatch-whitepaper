4f19f6801cf301ba3c97d56bc232e4054a1563cd
TheMadScore <TheMadScore@users.noreply.github.com>
2026-01-16 21:33:14 -0800

    fix: disable zoom on mobile

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
       attributes: {
         name: 'viewport',
         content:
-          'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
+          'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no',
       },
     },
   ],
